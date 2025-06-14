f41874039e1cce6d9e2d7c3524e2655a96e85583
Gangasagar-soni <sagarsoni39166@gmail.com>
2025-06-14 09:29:12 +0530

    remove default test and add test money

---
+++ tests-jasmin/moneyTest.js
@@ -0,0 +1,13 @@
+import {formatCurrency} from '../scripts/utils/money.js';
+
+describe('test suite: test formate currency',() => {
+  it('convert cents into doller',()=>{
+    expect(formatCurrency(2010)).toEqual('20.10');
+  });
+  it('working with 0',()=>{
+    expect(formatCurrency(2000)).toEqual('20.00');
+  });
+  it('testing with decimal after 4 numbers',()=>{
+    expect(formatCurrency(2000.1)).toEqual('20.00');
+  });
+});
---
+++ tests/moneyTest.js
@@ -0,0 +1,20 @@
+import formatCurrency from '../scripts/utils/money.js';
+console.log("test suite: formate currency")
+console.log("testing normal decimal")
+if (formatCurrency(2001) === '20.01'){
+  console.log("passed");
+}else {
+  console.log("failed");
+}
+console.log("testing wiht zero")
+if(formatCurrency(2000) === '20.00'){
+  console.log("passed");
+}else{
+  console.log("failed");
+}
+console.log("testing the decimal")
+if(formatCurrency(2000.1) === '20.00'){
+  console.log("passed");
+}else{
+  console.log("failed");
+}
